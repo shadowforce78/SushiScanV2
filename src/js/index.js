90911c58075baee800e81da48ee7d340388b9177
SaumonDeLuxe <planque.adam@gmail.com>
2025-06-30 19:25:38 +0200

    feat: enhance homepage layout and manga detail fetching; add info section and improve error handling in manga.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,36 @@
 const API_URL = 'https://api.saumondeluxe.com';
 
+
 async function tempHomepage() {
     const response = await fetch(`${API_URL}/scans/mangaList`);
     const data = await response.json();
-    console.log(data);
     return data.mangas; // Assuming the API returns an object with a 'mangas'
 }
 
+async function handleMangaClick(encodedTitle) {
+    const manga = await fetchMangaDetails(encodedTitle);
+    if (manga) {
+        displayMangaDetails(manga);
+    }
+}
+
 window.onload = async () => {
-    const mainDiv = document.getElementsByClassName('main')[0];
+    const homepageDiv = document.getElementsByClassName('homepage')[0];
+    const infoDiv = document.getElementsByClassName('info')[0];
     const mangaList = await tempHomepage();
     if (!mangaList || mangaList.length === 0) {
-        mainDiv.innerHTML = '<p>No mangas found.</p>';
+        homepageDiv.innerHTML = '<p>No mangas found.</p>';
         return;
     }
     mangaList.forEach(manga => {
         const mangaDiv = document.createElement('div');
         mangaDiv.className = `manga-${encodeURIComponent(manga.title)}`;
         mangaDiv.innerHTML = `
-            <button class="manga-button" onclick="fetchMangaDetails('${encodeURIComponent(manga.title)}')">
+            <button class="manga-button" onclick="handleMangaClick('${encodeURIComponent(manga.title)}')">
             <img src="${manga.image_url}" alt="${manga.title} cover" class="manga-cover">
             </button>
             <div class="manga-title">${manga.title}</div>
         `;
-        mainDiv.appendChild(mangaDiv);
+        homepageDiv.appendChild(mangaDiv);
     });
 }
